8cec057be294662f58d744d2567f24b53a3c57b9
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-01-11 20:31:55 +0800

    add more test

--- test/sanitize_sql_like_test.rb
+++ test/sanitize_sql_like_test.rb
@@ -7,7 +7,10 @@ class SanitizeSqlLikeTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::SanitizeSqlLike::VERSION
   end
-	def test_escape_percent_char
+	def test_escape_percent
     assert_equal('name\\%', User.sanitize_sql_like('name%'))
+  end
+  def test_escape_underline
+    assert_equal('.name\\_', User.sanitize_sql_like('.name_'))
   end
 end
