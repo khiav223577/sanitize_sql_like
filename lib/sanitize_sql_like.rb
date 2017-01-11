d32ae4bcf301e07745d2486a7ac85577a64d8b8b
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-01-11 21:06:32 +0800

    replace tab

--- lib/sanitize_sql_like.rb
+++ lib/sanitize_sql_like.rb
@@ -5,11 +5,11 @@ if Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new('4.2.1')
   module ActiveRecord
     module Sanitization
       module ClassMethods
-      	if not method_defined?(:sanitize_sql_like)
-	        def sanitize_sql_like(string, escape_character = "\\")
-	          pattern = Regexp.union(escape_character, "%", "_")
-	          string.gsub(pattern) { |x| [escape_character, x].join }
-	        end
+        if not method_defined?(:sanitize_sql_like)
+          def sanitize_sql_like(string, escape_character = "\\")
+            pattern = Regexp.union(escape_character, "%", "_")
+            string.gsub(pattern) { |x| [escape_character, x].join }
+          end
         end
       end
     end
