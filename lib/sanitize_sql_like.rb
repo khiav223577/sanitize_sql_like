bbeeec911759a488c58076fd46182842e930e86f
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-01-11 20:25:14 +0800

    add lib

--- lib/sanitize_sql_like.rb
+++ lib/sanitize_sql_like.rb
@@ -1,3 +1,17 @@
 require "sanitize_sql_like/version"
 require 'active_record'
 
+if Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new('4.2.1')
+  module ActiveRecord
+    module Sanitization
+      module ClassMethods
+      	if not method_defined?(:sanitize_sql_like)
+	        def sanitize_sql_like(string, escape_character = "\\")
+	          pattern = Regexp.union(escape_character, "%", "_")
+	          string.gsub(pattern) { |x| [escape_character, x].join }
+	        end
+        end
+      end
+    end
+  end
+end
