21a5763f1eb9f6c052ee0298c25cc00e307de676
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-08-12 04:17:44 +0800

    refactoring test cases

--- test/sanitize_sql_like_test.rb
+++ test/sanitize_sql_like_test.rb
@@ -1,15 +1,20 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class SanitizeSqlLikeTest < Minitest::Test
   def setup
 
   end
+
   def test_that_it_has_a_version_number
     refute_nil ::SanitizeSqlLike::VERSION
   end
+
   def test_escape_percent
     assert_equal('name\\%', User.send(:sanitize_sql_like, 'name%'))
   end
+
   def test_escape_underline
     assert_equal('.name\\_', User.send(:sanitize_sql_like, '.name_'))
   end
